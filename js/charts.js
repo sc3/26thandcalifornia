9910915ae059e30c6605a68850023d1ec2d98691
David Eads <davideads@gmail.com>
2013-12-04 00:01:10 -0600

    experimenting with line chart

--- js/charts.js
+++ js/charts.js
@@ -1,5 +1,7 @@
 /* Backbone wrapped D3 components */
-var Barchart = Backbone.View.extend({
+
+var JailView = Backbone.View.extend({
+  dimensions: {},
   initialize: function(o) {
     this.options = $.extend(true, {
       base_chart_height: 450,
@@ -14,9 +16,6 @@ var Barchart = Backbone.View.extend({
         left: 50
       }
     }, o);
-
-    this.dimensions = {};
-    this.collection.sort();
 
     $(window).on('resize', _.bind(this.render, this));
     this.render();
@@ -36,7 +35,10 @@ var Barchart = Backbone.View.extend({
     }
 
     this.dimensions.wrapperHeight = this.dimensions.height + this.options.margin.top + this.options.margin.bottom;
-  },
+  }
+});
+
+var Barchart = JailView.extend({
   render: function() {
     this.$el.empty();
     this.get_dimensions();
@@ -109,12 +111,24 @@ var Barchart = Backbone.View.extend({
     gy.selectAll("text")
         .attr("x", 4)
         .attr("dy", -4);
+
+    //create a function to draw a line using the scales
+    var line = d3.svg.line()
+      .x(function(d) { return xScale(new Date(d.date)); })
+      .y(function(d) { return yScale(+d.population); });
+
+    //draw the fever line
+    svg.append("path")
+      .datum(dataset)
+      .attr("class", "line")
+      .attr("d", line);
+
     //draw bars
-    svg.selectAll("rect")
-      .data(dataset)
-      .enter()
-      .append("rect")
-      .attr("fill", "#A1B7CB")
+    //svg.selectAll("rect")
+      //.data(dataset)
+      //.enter()
+      //.append("rect")
+      //.attr("fill", "#A1B7CB")
       //.on("mouseover", function(d) {
         //d3.select(this)
           //.style("fill", "#436e96");
@@ -150,16 +164,16 @@ var Barchart = Backbone.View.extend({
 
       //return tooltip.style("visibility", "hidden");
     //})
-    .attr("class", "bar")
-    .attr("text-anchor", "middle")
-    .attr("x", function(d) {
-        return xScale(new Date(d[options.xKey]));
-    })
-    .attr("y", function(d) {
-      return yScale(d[options.yKey]);
-    })
-    .attr("width", 0.75 * (dimensions.width / dataset.length) )
-    .attr("height", function(d) { return dimensions.height - yScale(d[options.yKey]) - 1; });
+    //.attr("class", "bar")
+    //.attr("text-anchor", "middle")
+    //.attr("x", function(d) {
+        //return xScale(new Date(d[options.xKey]));
+    //})
+    //.attr("y", function(d) {
+      //return yScale(d[options.yKey]);
+    //})
+    //.attr("width", 0.75 * (dimensions.width / dataset.length) )
+    //.attr("height", function(d) { return dimensions.height - yScale(d[options.yKey]) - 1; });
 
     svg.selectAll(".x.axis text")  // select all the text elements for the xaxis
         .attr("transform", function(d) {
@@ -171,4 +185,220 @@ var Barchart = Backbone.View.extend({
   }
 });
 
+var LineChart = JailView.extend({
+  render: function() {
+    this.$el.empty();
+    this.get_dimensions();
 
+    var dimensions = this.dimensions,
+        options = this.options,
+        dataset = this.collection.toJSON();
+
+    var yScale = d3.scale.linear()
+      .range([dimensions.height, 0]);
+
+    var xScale = d3.scale.linear()
+      .range([0, dimensions.width]);
+
+    //determine y maxium
+    var yMax = d3.max(dataset, function(d) { return d[yKey]; });
+
+    //create a function to draw a line using the scales
+    var line = d3.svg.line()
+      .x(function(d) { return xScale(d.yr); })
+      .y(function(d) { return yScale(d[lineKey]); });
+
+    var totalLine = d3.svg.line()
+      .x(function(d) { return xScale(d.yr); })
+      .y(function(d) { return yScale(d[yKey]); });
+
+    //create area 
+    var area = d3.svg.area()
+      .x(function(d) { return xScale(d.yr); })
+      .y0(dimensions.height)
+      .y1(function(d) { return yScale(d[yKey]); });
+
+    //create a new svg element
+    var svg = d3.select(container).append("svg")
+        .attr("width", dimensions.wrapperWidth)
+        .attr("height", dimensions.wrapperHeight)
+      .append("g")
+        .attr("transform", "translate(" + dimensions.margin.left + "," + dimensions.margin.top + ")");
+
+    //create axes
+    var xAxis = d3.svg.axis()
+        .scale(xScale)
+        .tickFormat(function(d) {
+          if ( d > 1995 && d !== 2000 && d !== 2005 && d !== 2010 )
+            return formatNumber(d).replace(/^(19|20)/g, "'");
+          return formatNumber(d);
+        })
+        .ticks(( $(container).width() <=550 )? 5:18 )
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(yScale)
+        .tickSize(dimensions.wrapperWidth)
+        .tickFormat(function(d) {
+          var ticks = svg.select('g.y.axis').selectAll('g')[0],
+              max_tick = d3.select(ticks[ticks.length - 1]).datum();
+
+          if ( Number(d) >= Number(max_tick) )
+            return formatBillions(d) + " billion";
+          else
+            return formatBillions(d);
+        })
+        .ticks(6)
+        .orient("right");
+
+    //set x and y scale values to map to the svg size
+    xScale.domain(d3.extent(dataset, function(d) { return d.yr; }));
+    yScale.domain([0, 14e9]);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + dimensions.height + ")")
+        .call(xAxis);
+
+    //create and set y axis positions
+    var gy = svg.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(-50, 0)")
+      .attr("y", 6)
+      .call(yAxis);
+
+    gy.selectAll("g").filter(function(d) { return d; })
+        .classed("minor", true);
+
+    gy.selectAll("text")
+        .attr("x", 4)
+        .attr("dy", -4);
+
+    //draw the area chart
+    svg.append("path")
+      .datum(dataset)
+      .attr("class", "area")
+      .attr("fill", "#c7d9e9")
+      .attr("opacity", ".3")
+      .attr("d", area);
+
+    //draw the fever line
+    svg.append("path")
+      .datum(dataset)
+      .attr("class", "line baseline")
+      .attr("d", line);
+
+    svg.append("path")
+      .datum(dataset)
+      .attr("class", "line total")
+      .style("stroke", "#A1B7CB")
+      .attr("d", totalLine);
+
+    //create a datapoint for each year on the line
+    circles_one = svg.selectAll("circle.one")
+      .data(dataset)
+      .enter()
+      .append("circle")
+      .on("mouseover", function(d){
+        d3.select(this)
+         .attr("r", 6);
+
+        //update tool tip text
+        d3.select("#tooltip")
+          .select(".bold")
+          .text(d.yr);
+
+        d3.select(".value")
+         .text("Principal: " + formatBigCurrency(d[lineKey]));
+
+        return tooltip.style("visibility", "visible");
+      })
+      .on("mousemove", function(){
+        var svg_width = d3.select("#chart-wrapper")[0][0].clientWidth;
+        var tip_width = $(tooltip[0][0]).width();
+
+        //set position of the tooltip based on mouse position
+        if (d3.event.offsetX > ((svg_width - tip_width))) {
+          return tooltip
+            .style("top", (d3.event.pageY-10)+"px")
+            .style("left",(d3.event.pageX-(tip_width+32))+"px");
+        } else {
+          return tooltip
+            .style("top", (d3.event.pageY-10)+"px")
+            .style("left",(d3.event.pageX+10)+"px");
+        }
+      })
+      .on("mouseout", function(d){
+         d3.select(this)
+          .attr("r", 3);
+
+          return tooltip.style("visibility", "hidden");
+      })
+      .attr("class", "dot baseline")
+      .attr("cx", function(d) { return xScale(d[xKey]); })
+      .attr("cy", function(d) { return yScale(d[lineKey]); })
+      .attr("r", 3);
+
+    circles_two = svg.selectAll("circle.two")
+      .data(dataset)
+      .enter()
+      .append("circle")
+      .on("mouseover", function(d){
+        d3.select(this)
+         .attr("r", 6);
+
+        //update tool tip text
+        d3.select("#tooltip")
+          .select(".bold")
+          .text(d.yr);
+
+        d3.select(".value")
+         .text("Total: " + formatBigCurrency(d[yKey]));
+
+        return tooltip.style("visibility", "visible");
+      })
+      .on("mousemove", function(){
+        var svg_width = d3.select("#chart-wrapper")[0][0].clientWidth;
+        var tip_width = $(tooltip[0][0]).width();
+
+        //set position of the tooltip based on mouse position
+        if (d3.event.offsetX > ((svg_width - tip_width))) {
+          return tooltip
+            .style("top", (d3.event.pageY-10)+"px")
+            .style("left",(d3.event.pageX-(tip_width+32))+"px");
+        } else {
+          return tooltip
+            .style("top", (d3.event.pageY-10)+"px")
+            .style("left",(d3.event.pageX+10)+"px");
+        }
+      })
+      .on("mouseout", function(d){
+         d3.select(this)
+          .attr("r", 3);
+
+          return tooltip.style("visibility", "hidden");
+      })
+      .attr("class", "dot total")
+      .attr("cx", function(d) { return xScale(d[xKey]); })
+      .attr("cy", function(d) { return yScale(d[yKey]); })
+      .attr("r", 3);
+
+    // Mouseover interaction with labels
+    var light_blue = $(container).parent().parent().find('.light-blue'),
+        blue = $(container).parent().parent().find('.blue');
+
+    light_blue.mouseover(function() {
+      svg.classed("highlight total", true);
+    }).mouseout(function() {
+      svg.classed("highlight total", false);
+    });
+
+    blue.mouseover(function() {
+      svg.classed("highlight baseline", true);
+    }).mouseout(function() {
+      svg.classed("highlight baseline", false);
+    });
+
+
+  }
+});
